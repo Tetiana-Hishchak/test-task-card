72e88b7e446922daf1a3a3af3d638915ad5ef162
Hishchak Tetiana <tanaiosered@ukr.net>
2024-06-08 20:46:20 +0300

    change function custom select

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,117 +7,110 @@ form.addEventListener('submit', (e) => {
   e.target.reset();
 });
 
-// const sort = document.getElementById('sort');
+function initializeDropDown(
+  dropDownWrapper, buttonSelector, listSelector,
+  listItemSelector, inputSelector, itemClickHandler,
+) {
+  const dropDownBtn = dropDownWrapper.querySelector(buttonSelector);
+  const dropDownList = dropDownWrapper.querySelector(listSelector);
+  const dropDownInput = dropDownWrapper.querySelector(inputSelector);
 
-// sort.addEventListener('change', function() {
-//   changeOptionColor('sort');
-// });
+  setupButtonToggle(dropDownBtn, dropDownList, buttonSelector, listSelector);
 
-// function changeOptionColor(name) {
-//   const select = document.getElementById(name);
-//   const options = select.options;
+  setupListItemClick(dropDownList, dropDownBtn, dropDownInput,
+    listItemSelector, listSelector, buttonSelector, itemClickHandler);
 
-//   for (let i = 0; i < options.length; i++) {
-//     if (options[i].selected && options[i].value !== '0') {
-//       options[i].style.color = '#df4e3c';
-//       select.classList.add('selected-border');
-//     } else {
-//       options[i].style.color = '#000000';
-//     }
-//   }
+  setupOutsideClick(dropDownWrapper, dropDownBtn,
+    dropDownList, buttonSelector, listSelector);
 
-//   if (select.value === '0') {
-//     select.classList.remove('selected-border');
-//   }
-// }
+  setupKeyboardNavigation(dropDownBtn, dropDownList,
+    buttonSelector, listSelector);
+}
 
-document.querySelectorAll('.dropdown').forEach(dropDownWrapper => {
-  const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
-  const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
-  const dropDownListItems = dropDownList
-    .querySelectorAll('.dropdown__list-item');
-  const dropDownInput = dropDownWrapper
-    .querySelector('.dropdown__input-hidden');
+function setupButtonToggle(dropDownBtn, dropDownList,
+  buttonSelector, listSelector) {
+  const visibleClass = `${listSelector.substring(1)}--visible`;
+  const activeClass = `${buttonSelector.substring(1)}--active`;
 
-  dropDownBtn.addEventListener('click', e => {
-    dropDownList.classList.toggle('dropdown__list--visible');
-    dropDownBtn.classList.toggle('dropdown__button--active');
+  dropDownBtn.addEventListener('click', () => {
+    dropDownList.classList.toggle(visibleClass);
+    dropDownBtn.classList.toggle(activeClass);
   });
+}
 
-  dropDownListItems.forEach(listItem => {
-    listItem.addEventListener('click', e => {
+function setupListItemClick(
+  dropDownList, dropDownBtn, dropDownInput, listItemSelector, listSelector,
+  buttonSelector, itemClickHandler,
+) {
+  const visibleClass = `${listSelector.substring(1)}--visible`;
+
+  dropDownList.addEventListener('click', e => {
+    if (e.target.matches(listItemSelector)) {
       e.stopPropagation();
-      dropDownBtn.innerText = listItem.innerText;
+      dropDownBtn.innerText = e.target.innerText;
       dropDownBtn.focus();
-      dropDownInput.value = listItem.dataset.value;
-      dropDownList.classList.remove('dropdown__list--visible');
-      dropDownListItems.forEach(item => item.classList.remove('selected'));
-      listItem.classList.add('selected');
+      dropDownInput.value = e.target.dataset.value;
+      dropDownList.classList.remove(visibleClass);
 
-      filterCards(dropDownBtn.id, listItem.dataset.value);
-    });
+      dropDownList.querySelectorAll(listItemSelector)
+        .forEach(item => item.classList.remove('selected'));
+      e.target.classList.add('selected');
+      itemClickHandler(dropDownBtn.id, e.target);
+    }
   });
+}
+
+function setupOutsideClick(
+  dropDownWrapper, dropDownBtn, dropDownList, buttonSelector, listSelector,
+) {
+  const visibleClass = `${listSelector.substring(1)}--visible`;
+  const activeClass = `${buttonSelector.substring(1)}--active`;
 
   document.addEventListener('click', e => {
     if (!dropDownWrapper.contains(e.target)) {
-      dropDownBtn.classList.remove('dropdown__button--active');
-      dropDownList.classList.remove('dropdown__list--visible');
+      dropDownBtn.classList.remove(activeClass);
+      dropDownList.classList.remove(visibleClass);
     }
   });
+}
+
+function setupKeyboardNavigation(
+  dropDownBtn, dropDownList, buttonSelector, listSelector,
+) {
+  const visibleClass = `${listSelector.substring(1)}--visible`;
+  const activeClass = `${buttonSelector.substring(1)}--active`;
 
   document.addEventListener('keydown', e => {
     if (e.key === 'Tab' || e.key === 'Escape') {
-      dropDownBtn.classList.remove('dropdown__button--active');
-      dropDownList.classList.remove('dropdown__list--visible');
+      dropDownBtn.classList.remove(activeClass);
+      dropDownList.classList.remove(visibleClass);
     }
   });
+}
 
-  function filterCards(elementId, value) {
-    const cards = document.querySelectorAll('.card');
-    const conditionFunctions = {
-      year: (selected, card) => selected === 'year' || +selected === +card,
-      model: (selected, card) => selected === 'model' || selected === card,
-      mileage: (selected, card) => selected === 'mileage' || +selected >= +card,
-    };
+document.querySelectorAll('.dropdown').forEach(dropDownWrapper => {
+  initializeDropDown(
+    dropDownWrapper,
+    '.dropdown__button',
+    '.dropdown__list',
+    '.dropdown__list-item',
+    '.dropdown__input-hidden',
+    (elementId, listItem) => {
+      const value = listItem.dataset.value;
 
-    const conditionFunction = conditionFunctions[elementId];
-
-    if (conditionFunction) {
-      cards.forEach(card => {
-        const cardValue = card.getAttribute(`data-${elementId}`);
-
-        card.style.display = conditionFunction(value, cardValue)
-          ? 'block' : 'none';
-      });
-    }
-  }
+      filterCards(elementId, value);
+    },
+  );
 });
 
 document.querySelectorAll('.sort').forEach(dropDownWrapper => {
-  const dropDownBtn = dropDownWrapper.querySelector('.sort__button');
-  const dropDownList = dropDownWrapper.querySelector('.sort__list');
-  const dropDownListItems = dropDownList
-    .querySelectorAll('.sort__list-item');
-  const dropDownInput = dropDownWrapper
-    .querySelector('.sort__input-hidden');
-  const sortContainer = document.querySelector('.product');
-  const sortContent = '.card';
-
-  dropDownBtn.addEventListener('click', e => {
-    dropDownList.classList.toggle('sort__list--visible');
-    dropDownBtn.classList.toggle('sort__button--active');
-  });
-
-  dropDownListItems.forEach(listItem => {
-    listItem.addEventListener('click', e => {
-      e.stopPropagation();
-      dropDownBtn.innerText = listItem.innerText;
-      dropDownBtn.focus();
-      dropDownInput.value = listItem.dataset.value;
-      dropDownList.classList.remove('sort__list--visible');
-      dropDownListItems.forEach(item => item.classList.remove('selected'));
-      listItem.classList.add('selected');
-
+  initializeDropDown(
+    dropDownWrapper,
+    '.sort__button',
+    '.sort__list',
+    '.sort__list-item',
+    '.sort__input-hidden',
+    (elementId, listItem) => {
       const dataType = listItem.getAttribute('data-type');
       const sortOrder = listItem.getAttribute('data-order');
       const sortIndicator = document.getElementById('sortIndicator');
@@ -131,45 +124,54 @@ document.querySelectorAll('.sort').forEach(dropDownWrapper => {
       }
 
       sortElements(dataType, sortOrder);
-    });
-  });
-
-  function sortElements(dataType, sortOrder) {
-    const elements = Array.from(sortContainer.querySelectorAll(sortContent));
-
-    elements.sort((a, b) => {
-      let aValue = a.getAttribute(`data-${dataType}`);
-      let bValue = b.getAttribute(`data-${dataType}`);
-
-      if (dataType === 'price') {
-        aValue = parseFloat(aValue);
-        bValue = parseFloat(bValue);
-      } else if (dataType === 'name') {
-        aValue = aValue.toString().toLowerCase();
-        bValue = bValue.toString().toLowerCase();
-      }
-
-      if (sortOrder === 'asc') {
-        return (aValue > bValue) ? 1 : (aValue < bValue) ? -1 : 0;
-      } else if (sortOrder === 'desc') {
-        return (aValue < bValue) ? 1 : (aValue > bValue) ? -1 : 0;
-      }
-    });
-
-    elements.forEach(element => sortContainer.appendChild(element));
-  }
-
-  document.addEventListener('click', e => {
-    if (!dropDownWrapper.contains(e.target)) {
-      dropDownBtn.classList.remove('sort__button--active');
-      dropDownList.classList.remove('sort__list--visible');
-    }
-  });
-
-  document.addEventListener('keydown', e => {
-    if (e.key === 'Tab' || e.key === 'Escape') {
-      dropDownBtn.classList.remove('sort__button--active');
-      dropDownList.classList.remove('sort__list--visible');
-    }
-  });
+    },
+  );
 });
+
+function filterCards(elementId, value) {
+  const cards = document.querySelectorAll('.card');
+
+  const conditionFunctions = {
+    year: (selected, card) => selected === 'year' || +selected === +card,
+    model: (selected, card) => selected === 'model' || selected === card,
+    mileage: (selected, card) => selected === 'mileage' || +selected >= +card,
+  };
+
+  const conditionFunction = conditionFunctions[elementId];
+
+  if (conditionFunction) {
+    cards.forEach(card => {
+      const cardValue = card.getAttribute(`data-${elementId}`);
+
+      card.style.display = conditionFunction(value, cardValue)
+        ? 'block' : 'none';
+    });
+  }
+}
+
+function sortElements(dataType, sortOrder) {
+  const sortContainer = document.querySelector('.product');
+  const sortContent = '.card';
+  const elements = Array.from(sortContainer.querySelectorAll(sortContent));
+
+  elements.sort((a, b) => {
+    let aValue = a.getAttribute(`data-${dataType}`);
+    let bValue = b.getAttribute(`data-${dataType}`);
+
+    if (dataType === 'price') {
+      aValue = parseFloat(aValue);
+      bValue = parseFloat(bValue);
+    } else if (dataType === 'name') {
+      aValue = aValue.toString().toLowerCase();
+      bValue = bValue.toString().toLowerCase();
+    }
+
+    if (sortOrder === 'asc') {
+      return (aValue > bValue) ? 1 : (aValue < bValue) ? -1 : 0;
+    } else if (sortOrder === 'desc') {
+      return (aValue < bValue) ? 1 : (aValue > bValue) ? -1 : 0;
+    }
+  });
+
+  elements.forEach(element => sortContainer.appendChild(element));
+}
